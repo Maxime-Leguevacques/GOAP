39bea1497671eb78a4d2734d659c83f53ce30719
Maxime Leguevacques <m.leguevacques@student.isartdigital.com>
2025-06-06 14:44:00 +0200

    initial map

---
+++ Assets/Scripts/Camera/Camera.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    [SerializeField] private GameObject m_agent;
+    [SerializeField] private float smoothSpeed = 5f;
+
+    private Vector3 m_offset;
+
+    void Start()
+    {
+        if (m_agent != null)
+        {
+            m_offset = transform.position - m_agent.transform.position;
+        }
+    }
+
+    void LateUpdate()
+    {
+        if (m_agent == null) return;
+
+        Vector3 targetPosition = new Vector3(
+            m_agent.transform.position.x + m_offset.x,
+            transform.position.y, // Keep Y fixed
+            m_agent.transform.position.z + m_offset.z
+        );
+
+        transform.position = Vector3.Lerp(transform.position, targetPosition, smoothSpeed * Time.deltaTime);
+    }
+    
+}
